335d22dd3a933a455fda1167560381bede6bc0c7
SelesCalin <calinseles30@gmail.com>
2023-02-21 18:11:36 +0200

    Real serialVersionUid

--- github-retrieve/src/main/java/com/example/githubretrieve/exception/UserNotFoundException.java
+++ github-retrieve/src/main/java/com/example/githubretrieve/exception/UserNotFoundException.java
@@ -1,9 +1,8 @@
 package com.example.githubretrieve.exception;
 
 import java.io.Serial;
-import java.io.Serializable;
 
-public class UserNotFoundException extends RuntimeException implements Serializable {
+public class UserNotFoundException extends RuntimeException {
 
     @Serial
     private static final long serialVersionUID = -6165718301762435365L;
